d917870386d377aa08c175f773fe0298a40d7af9
cxinu <72374438+cxinu@users.noreply.github.com>
2025-05-31 13:17:24 +0530

    migration and Dockerize

--- backend/utils/jwt.go
+++ backend/utils/jwt.go
@@ -13,5 +13,5 @@ func GenerateToken(username string) (string, error) {
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	return token.SignedString([]byte(os.Getenv("APP_JWT_SECRET")))
 }
