daa83b5d7eed4f84b41bd7621a7534d36dbb661b
cxinu <72374438+cxinu@users.noreply.github.com>
2025-06-01 00:16:49 +0530

    add CORS middleware

---
+++ backend/middleware/cors.go
@@ -0,0 +1,18 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func CORSMiddleware() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	})
+}
