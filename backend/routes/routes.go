daead713ba21c669a21768a9f31f7464f596ce27
cxinu <72374438+cxinu@users.noreply.github.com>
2025-06-01 17:25:36 +0530

    rm trailing `/` in endpoints

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -20,32 +20,32 @@ func RegisterRoutes(r *gin.Engine) {
 
 		course := api.Group("/course")
 		{
-			course.GET("/", controllers.All[models.Course](db))
-			course.POST("/", controllers.Create[models.Course](db))
+			course.GET("", controllers.All[models.Course](db))
+			course.POST("", controllers.Create[models.Course](db))
 			course.GET("/:id", controllers.Get[models.Course](db))
 			course.PUT("/:id", controllers.Update[models.Course](db))
 			course.DELETE("/:id", controllers.Delete[models.Course](db))
 		}
 		subject := api.Group("/subject")
 		{
-			subject.GET("/", controllers.All[models.Subject](db))
-			subject.POST("/", controllers.All[models.Subject](db))
+			subject.GET("", controllers.All[models.Subject](db))
+			subject.POST("", controllers.All[models.Subject](db))
 			subject.GET("/:id", controllers.Get[models.Subject](db))
 			subject.PUT("/:id", controllers.Update[models.Subject](db))
 			subject.DELETE("/:id", controllers.Delete[models.Subject](db))
 		}
 		faculty := api.Group("/faculty")
 		{
-			faculty.GET("/", controllers.All[models.Faculty](db))
-			faculty.POST("/", controllers.Create[models.Faculty](db))
+			faculty.GET("", controllers.All[models.Faculty](db))
+			faculty.POST("", controllers.Create[models.Faculty](db))
 			faculty.GET("/:id", controllers.Get[models.Faculty](db))
 			faculty.PUT("/:id", controllers.Update[models.Faculty](db))
 			faculty.DELETE("/:id", controllers.Delete[models.Faculty](db))
 		}
 		room := api.Group("/room")
 		{
-			room.GET("/", controllers.All[models.Room](db))
-			room.POST("/", controllers.Create[models.Room](db))
+			room.GET("", controllers.All[models.Room](db))
+			room.POST("", controllers.Create[models.Room](db))
 			room.GET("/:id", controllers.Get[models.Room](db))
 			room.PUT("/:id", controllers.Update[models.Room](db))
 			room.DELETE("/:id", controllers.Delete[models.Room](db))
@@ -54,16 +54,16 @@ func RegisterRoutes(r *gin.Engine) {
 		// WARNING: Experimental: Using generic controllers User and Timetable
 		user := api.Group("/user")
 		{
-			user.GET("/", controllers.All[models.User](db))
-			user.POST("/", controllers.Create[models.User](db))
+			user.GET("", controllers.All[models.User](db))
+			user.POST("", controllers.Create[models.User](db))
 			user.GET("/:id", controllers.Get[models.User](db))
 			user.PUT("/:id", controllers.Update[models.User](db))
 			user.DELETE("/:id", controllers.Delete[models.User](db))
 		}
 		timetable := api.Group("/timetable")
 		{
-			timetable.GET("/", controllers.All[models.Timetable](db))
-			timetable.POST("/", controllers.Create[models.Timetable](db))
+			timetable.GET("", controllers.All[models.Timetable](db))
+			timetable.POST("", controllers.Create[models.Timetable](db))
 			timetable.GET("/:id", controllers.Get[models.Timetable](db))
 			timetable.PUT("/:id", controllers.Update[models.Timetable](db))
 			timetable.DELETE("/:id", controllers.Delete[models.Timetable](db))
