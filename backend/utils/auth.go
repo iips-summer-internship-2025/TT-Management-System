21661087629795d0aa9ec4aa0e7e2578230fc583
harshali <harshali.9826@gmail.com>
2025-06-04 17:15:39 +0530

    feat: password hashing with bcrypt and authentication

--- backend/utils/auth.go
+++ backend/utils/auth.go
@@ -4,19 +4,31 @@ import (
 	"errors"
 	"tms-server/config"
 	"tms-server/models"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
-// TODO: hash password with bcrypt
-func AuthenticateUser(username, password string) (*models.User, error) {
+// AuthenticateUser checks if username exists in database
+func AuthenticateUser(username string) (*models.User, error) {
 	var user models.User
 
 	if err := config.DB.Where("username = ?", username).First(&user).Error; err != nil {
 		return nil, errors.New("invalid username or password")
 	}
 
-	if user.Password != password {
-		return nil, errors.New("invalid username or password")
-	}
-
 	return &user, nil
+}
+
+// HashPassword hashes the password using bcrypt
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
+
+// CheckPassword compares a password against a hash
+func CheckPassword(password, hash string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 }
