98b8b61c730d4e452f6329ec202fd3e729b9b192
Somya0827 <somyashukla2702@gmail.com>
2025-06-14 01:28:36 +0530

    changes done for faculty view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,17 @@ import "./App.css";
 import { AuthProvider } from "./context/AuthContext";
 import Layout from "./components/Layout";
 import AppRoutes from "./routes/AppRoutes";
+import { UserRoleProvider } from './context/UserRoleContext';
 
 const App = () => {
   return (
+    <UserRoleProvider>
     <AuthProvider>
       <Layout>
         <AppRoutes />
       </Layout>
     </AuthProvider>
+    </UserRoleProvider>
   );
 };
 
